9aa9ca5b70e15f44d94d96b21c4547fd5748aa74
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-25 13:52:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+const odi = 'Odie';
+ const append = ["Milo", "Otis", "Garfield"];
+ let prepend = append.slice();
+ let removeLast = append.slice();
+ let removeFirst = append.slice();
+ removeFirst.shift();
+ prepend.unshift(odi);
+ removeLast.pop();
+ append.push(odi);
+
+
